b2f1209de0a17e81db021008afaf88df777a0010
liuhong <574231288@qq.com>
2018-05-23 12:01:09 +0800

    对反射进行测试.配置了AOP标签声明

--- src/cn/dw/service/ProductServiceImpl.java
+++ src/cn/dw/service/ProductServiceImpl.java
@@ -14,6 +14,7 @@ public class ProductServiceImpl {
 	
 	// 此数据会从显示层传入
 	public int save(Product product) {
+		// commit  rollback
 		// 目前service并没有复杂的业务逻辑,因此仅仅调用数据访问层
 		int result = productDao.save(product);
 		// 模拟事务操作: 希望当前逻辑多个操作是一个整体(只要有异常则操作回滚)
