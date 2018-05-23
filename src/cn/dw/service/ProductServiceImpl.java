0d5c6811da8c6283d64ad398ae3b526af8d052d9
liuhong <574231288@qq.com>
2018-05-23 13:00:50 +0800

    1: 此版本添加Service,通过测试发现,目前JDBC每个独立的操作都是一个事务(在业务逻辑后面代码抛出异常,不会导致前面操提交) 2: 在spring-bean.xml中已经导入aop声明

---
+++ src/cn/dw/service/ProductServiceImpl.java
@@ -0,0 +1,42 @@
+package cn.dw.service;
+
+import cn.dw.dao.ProductDaoImpl;
+import cn.dw.model.Product;
+
+// 项目的业务逻辑实现,都被封装在Service层,此层不能有访问数据库的代码,如果需要进行数据库操作,则应该Service-->Dao
+public class ProductServiceImpl {
+	// spring通过xml的配置取代了硬编码,依赖都要交给Spring管理
+	private ProductDaoImpl productDao = null;
+	// 通过set注入
+	public void setProductDao(ProductDaoImpl productDao) {
+		this.productDao = productDao;
+	}
+	
+	// 此数据会从显示层传入
+	public int save(Product product) {
+		// commit  rollback
+		// 目前service并没有复杂的业务逻辑,因此仅仅调用数据访问层
+		int result = productDao.save(product);
+		// 模拟事务操作: 希望当前逻辑多个操作是一个整体(只要有异常则操作回滚)
+//		Integer.parseInt("xxxx");
+		return result;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
