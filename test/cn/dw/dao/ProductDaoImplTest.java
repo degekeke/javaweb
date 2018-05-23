c17c61a7f15ce80c8fca0760415d8bdae8b31270
liuhong <574231288@qq.com>
2018-05-23 10:14:08 +0800

    此版本添加Service,通过测试发现,目前JDBC每个独立的操作都是一个事务(在业务逻辑后面代码抛出异常,不会导致前面操作回滚)

--- test/cn/dw/dao/ProductDaoImplTest.java
+++ test/cn/dw/dao/ProductDaoImplTest.java
@@ -11,11 +11,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import cn.dw.model.Product;
+import cn.dw.service.ProductServiceImpl;
 
 public class ProductDaoImplTest {
 
 	// static说明当前属性是类属性,只有一份,静态方法只能操作静态属性
-	private static ProductDaoImpl daoImpl = null;
+	private static ProductServiceImpl serviceImpl = null;
 	private static ApplicationContext context = null;
 
 	@BeforeClass
@@ -23,29 +24,29 @@ public class ProductDaoImplTest {
 		System.out.println("setUpBeforeClass.....");
 		context = new ClassPathXmlApplicationContext("spring-bean.xml");
 //		daoImpl = (ProductDaoImpl)context.getBean("productDao");
-		daoImpl = context.getBean("productDao", ProductDaoImpl.class);
+		serviceImpl = context.getBean("productService", ProductServiceImpl.class);
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		System.out.println("tearDownAfterClass.....");
-		daoImpl = null;
+		serviceImpl = null;
 	}
 
-	@Test
-	public void testQueryByName() {
-		// "%%":代表查询所有记录
-		List<Product> proList = daoImpl.queryByName("",2,3);
-		for (Product temp : proList) {
-			System.out.println(temp + "=======>" + temp.toString());
-		}
-	}
+//	@Test
+//	public void testQueryByName() {
+//		// "%%":代表查询所有记录
+//		List<Product> proList = serviceImpl.queryByName("",2,3);
+//		for (Product temp : proList) {
+//			System.out.println(temp + "=======>" + temp.toString());
+//		}
+//	}
 //	
-	@Test
-	public void testGetById() {
-		// "%%":代表查询所有记录
-		System.out.println(daoImpl.getById(4));
-	}
+//	@Test
+//	public void testGetById() {
+//		// "%%":代表查询所有记录
+//		System.out.println(serviceImpl.getById(4));
+//	}
 
 	@Test
 	public void testDelete() {
@@ -61,10 +62,10 @@ public class ProductDaoImplTest {
 	public void testSave() {
 		System.out.println("save........");
 		Product product = new Product();
-		product.setName("xyz~~~~~");
+		product.setName("事务测试");
 		product.setPrice(3.14);
 		product.setRemark("remark");
-		daoImpl.save(product);
+		serviceImpl.save(product);
 	}
 
 }
